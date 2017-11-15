ce85bd5b1e99fb29b959f253be02ecf3a00d9fa9
Cee <c.weitian.ng@gmail.com>
2017-11-15 16:27:19 -0500

    added a typewriter animation for text

--- app/src/main/kotlin/cee/ng/aliceadventure/screens/textadventure/startadventure/TextAdventureStartFragment.kt
+++ app/src/main/kotlin/cee/ng/aliceadventure/screens/textadventure/startadventure/TextAdventureStartFragment.kt
@@ -1,24 +1,36 @@
 package cee.ng.aliceadventure.screens.textadventure.startadventure
 
+import android.os.Bundle
+import butterknife.BindView
 import cee.ng.aliceadventure.R
 import cee.ng.aliceadventure.base.BaseFragment
 import cee.ng.aliceadventure.base.PresenterConfiguration
+import cee.ng.aliceadventure.customviews.TypeWriter
 
-class TextAdventureStartFragment: BaseFragment<TextAdventureStartContract.Presenter>(), TextAdventureStartContract.View {
+class TextAdventureStartFragment : BaseFragment<TextAdventureStartContract.Presenter>(), TextAdventureStartContract.View {
 
+    @BindView(R.id.text_adventure_text)
+    lateinit var storyText: TypeWriter
     override val layoutResourceId: Int = R.layout.fragment_text_adventure_start
 
     companion object {
-    val TAG = TextAdventureStartFragment::class.java.simpleName
+        val TAG = TextAdventureStartFragment::class.java.simpleName
 
-    fun newInstance(): TextAdventureStartFragment {
-        val fragment = TextAdventureStartFragment()
-        return fragment
-    }
+        fun newInstance(): TextAdventureStartFragment {
+            val fragment = TextAdventureStartFragment()
+            return fragment
+        }
 
     }
 
     override fun createPresenter(configuration: PresenterConfiguration): TextAdventureStartContract.Presenter {
         return TextAdventureStartPresenter(this, configuration)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        storyText.text = ""
+        storyText.setCharacterDelay(100)
+        storyText.animateText("Alice, it's times to wake up!")
     }
 }
